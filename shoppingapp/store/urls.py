6278951c85c0774e660fa41671deed480ff1c142
shyamlal-as <shyamlal@qburst.com>
2020-12-02 13:52:56 +0530

    Products and login page

--- shoppingapp/store/urls.py
+++ shoppingapp/store/urls.py
@@ -3,5 +3,10 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.Home, name='store-home'),
+    #path('', views.Home, name='store-home'),
+
+    path('',views.store,name="store"),
+    path('cart/',views.cart,name="cart"),
+    path('checkout/',views.checkout,name="checkout"),
+    path('product/',views.product,name="product"),
 ]
