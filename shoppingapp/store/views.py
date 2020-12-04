608d79b39df26fc412028021e2d82ad7a8589fe1
RahulN-git <rahulnagesh@qburst.com>
2020-12-04 13:17:41 +0530

    Added search bar and Pagination

--- shoppingapp/store/views.py
+++ shoppingapp/store/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render
 from .models import Categories,Product
-
+from django.http import HttpResponse
+from django.db.models import Q
+from django.core.paginator import Paginator
 # Create your views here.
 
 def store(request):
 	products=Product.objects.all()
-	context={}
-	return render(request,'store/store.html',{'products':products})
+	product_paginator=Paginator(products,6)
+	page_num=request.GET.get('page')
+	page=product_paginator.get_page(page_num)
+	context={
+		'page':page,
+	}
+	return render(request,'store/store.html',context)
 
 def cart(request):
+	print('Cart')
 	context={}
 	return render(request,'store/cart.html',context)
 
@@ -17,9 +25,20 @@ def checkout(request):
 	return render(request,'store/checkout.html',context)
 
 def product(request,categories_id):
+	print('getting in')
 	products=Product.objects.filter(categories_id=categories_id)
 	return render(request,'store/prod.html',{'products':products})
 
 def category(request):
 	lists=Categories.objects.all()
 	return render(request,'store/first.html',{'lists':lists})
+
+def search(request):
+	if request.method=='POST':
+		search=request.POST['search']
+		print('search')
+		product=Product.objects.all().filter(Q(name__icontains=search))
+		
+		return render(request,'store/store.html',{'products':product})
+	else:
+		return render(request,'store/store.html')
