3c23125d2d6967dd71f42d2398e051549c6c0e8c
shyamlal-as <shyamlal@qburst.com>
2020-12-24 13:14:45 +0530

    Directory structure for versioning

--- shoppingapp/shoppingapp/urls.py
+++ shoppingapp/shoppingapp/urls.py
@@ -22,6 +22,9 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
+
+
+
     path('admin/', admin.site.urls),
     path('', include('store.urls')),
     #path('cart/',include('purchases.urls')),
